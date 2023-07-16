45d76addbd4f74629e6b29ffa27450f96e789deb
CarlosNunezMX <carlitosnu41@gmail.com>
2023-07-16 17:55:13 -0600

    Finished home page method

--- source/HomePage.js
+++ source/HomePage.js
@@ -1,62 +1,105 @@
 import { Urls, getResource } from "./connectors/makeFetchHappend.js";
-export async function Home({lang}){
+export async function Home({ lang }) {
     const url = Urls[lang];
     const $ = await getResource({ url, domParse: true });
-    
-    const PopularBooks = ParseBooks($);
-
-    return {
-        PopularBooks
-    }
-}
-
-function ParseBooks($){
-    const PopularBooks = []
-    const $PopularBooks = $(".index-carousel .carousel-list .book-item");
-
-    $PopularBooks.each((_, _el) => {
-        const $Book = $(_el);
-        const bookID = $Book.find(".book-pic a[itemprop='url']")
+    const Sections = [];
+    // Get Book Sections (.index-book-list)
+    const $BookSections = $(".index-book-list")
+    $BookSections.each((_, _el) => {
+        const $Section = $(_el);
+        let Title = $Section.find("a")
             .first()
-            .attr("href")
-            .replace(".html", "")
-            .split("novel/")[1];
+            .attr("title")
+            .split(" ", 2)[1]
+        const Books = [];
+        // GetBooks
+        const $Books = $Section.find(".book-list .book-item")
+        $Books.each((__, _book) => {
+            /**
+             * @type {import("./index.js").SectionResultBook}
+             */
+            let book = {}
+            const $book = $(_book);
+            const $a = $book.find("a[itemprop='url']")
+                .first();
 
-        const bookImage = $Book.find("a img[itemprop='image']")
-            .first()
-            .attr("src");
-
-        let bookReview = $Book.find("a .book-rate-num-circle")    
-            .first()
-            .text();
-
-        bookReview = bookReview !== 0 ? Number(bookReview) : 0;
-
-        let bookDescription = $Book.find(".book-summary .book-summary-content")
-            .first()
-            .text()
-            .trim()
-
-        const bookName = $Book.find(".book-name")
-            .first()
-            .text()
-        
-        let bookViews = $Book.find(".book-info .book-data .book-data-item .book-data-num")
-            .first()
-            .text()
-            .replace(",", "")
-
-        bookViews = 
-            (bookViews === "" || bookViews === "N/A") ? 0 : Number(bookViews)
-        PopularBooks.push({
-            bookID, 
-            bookImage, 
-            bookReview, 
-            bookDescription, 
-            bookName,
-            bookViews
+            book.Name = $book.find("div[itemprop='name']")
+                .first()
+                .text();
+            book.Id = $a
+                .attr("href")
+                .split("/novel/")[1]
+                .replace(".html", "");
+            book.Review = $book.find(".book-rate .book-rate-num")
+                .first()
+                .text()
+            book.Image = $book.find(".book-pic img")
+                .attr("src");
+            book.Review = (book.Review !== "") ? Number(book.Review) : 0;
+            book.Type = $book.find(".book-type")
+                .first()
+                .text()
+            book.Description = $book.find(".book-summary .book-summary-content")
+                .first()
+                .text()
+            book.Views = $book.find(".book-data .book-data-item span.book-data-num")
+                .first()
+                .text()
+                .replace(",", "")
+            book.Views = (book.Views !== "") ? Number(book.Views) : 0;
+            book.Date = $book.find(".book-data .book-data-item .book-data-time")
+                .first()
+                .text()
+            Books.push(book)
+        })
+        Sections.push({
+            Title,
+            Books
         })
     })
 
-    return PopularBooks;
+
+    const $carrousel = $(".index-carousel")
+    const Title = $carrousel.find(".carousel-title")
+        .text()
+
+    const $Items = $carrousel.find(".carousel-list .book-item")
+    const PopularBooks = [];
+    $Items.each((_, _el) => {
+        /** @type {import("./index.js").SectionResultBook} */
+        let book = {};
+        const $book = $(_el);
+        const $Id = $book.find(".book-info a")
+
+        book.Id = $Id.attr("href")
+            .split("/novel/")[1]
+            .replace(".html", "")
+        book.Name = $Id.attr("title");
+
+        book.Views = $book.find(".book-data .book-data-num")
+            .first()    
+            .text()
+            .replace(",", "")
+        book.Views = (book.Views !== "") ? Number(book.Views) : 0;
+
+        // Summary
+        book.Description = $book.find(".book-summary-content")
+            .first()
+            .text()
+
+        book.Review = $book.find(".book-rate-num-circle")
+            .first()
+            .text()
+
+        book.Review = (book.Review !== "") ? Number(book.Review) : 0;
+        book.Image = $book.find(".book-pic img")
+            .first()
+            .attr("src")
+        PopularBooks.push(book)
+    })
+    Sections.push({
+        Title,
+        Books: PopularBooks
+    })
+    return Sections
 }
